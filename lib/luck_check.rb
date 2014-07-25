09f3f20aa2210d286ed547319c9be39e26d4c2a1
Justin Pacubas & jennifermccarthy <student@epicodus.com>
2014-07-25 16:25:15 -0700

    Completed functionality and refactored.

--- lib/luck_check.rb
+++ lib/luck_check.rb
@@ -1,13 +1,44 @@
 def luck_check(input_number)
-  ticket = (input_number.to_s).split("")
-  ticket1 = ticket.slice((ticket.length/2 - ticket.length/2)..ticket.length/2-1)
-  ticket2 = ticket.slice((ticket.length/2)..-1)
+  ticket = (input_number.to_s)
 
-  ticket1 = ticket1.to_s.scan(/\d/).map {|i| i.to_i}
-  ticket2 = ticket2.to_s.scan(/\d/).map {|i| i.to_i}
+  if ticket.length.even?
+    ticket = ticket.split("")
+    ticket1 = ticket.slice((ticket.length/2 - ticket.length/2)..ticket.length/2-1).map(&:to_i)
+    ticket2 = ticket.slice((ticket.length/2)..-1).map(&:to_i)
 
-  print ticket1
-  print ticket2
+    #refactored on lines 6 & 7
+    # ticket1 = ticket1.map(&:to_i)
+    # ticket2 = ticket2.to_s.scan(/\d/).map {|i| i.to_i}
+
+    return ticket1.inject(:+) == ticket2.inject(:+)
+  else
+    ticket = ticket<<"0"
+    ticket = ticket.split("")
+
+    ticket1 = ticket.slice((ticket.length/2 - ticket.length/2)..ticket.length/2-1).map(&:to_i)
+    ticket2 = ticket.slice((ticket.length/2)..-1).map(&:to_i)
+
+    ticket1.pop
+    ticket2.pop
+
+    return ticket1.inject(:+) == ticket2.inject(:+)
+
+    #refactored out on lines 18 & 19
+    # ticket1 = ticket1.to_s.scan(/\d/).map {|i| i.to_i}
+    # ticket2 = ticket2.to_s.scan(/\d/).map {|i| i.to_i}
+
+    #refactored out on line 24
+    # sum_ticket1 = ticket1.inject{|sum,x| sum + x}
+    # sum_ticket2 = ticket2.inject{|sum,x| sum + x}
+  end
+
+  #refactored out on lines 13 & 24
+  # if sum_ticket1==sum_ticket2
+  #   true
+  # else
+  #   false
+  # end
+
 end
 
-print luck_check(260053)
+print luck_check(26053)
--- spec/luck_check_spec.rb
+++ spec/luck_check_spec.rb
@@ -3,6 +3,18 @@ require ("luck_check")
 
 describe("luck_check") do
   it("outputs true for a given even number if it is considered lucky") do
-    luck_check(2608).should(eq(true))
+    luck_check(2653).should(eq(true))
+  end
+
+  it("outputs true for a given odd number if it is considered lucky") do
+    luck_check(26053).should(eq(true))
+  end
+
+  it("outputs false for a given even number if it is not considered lucky") do
+    luck_check(2615).should(eq(false))
+  end
+
+  it("outputs false for a given odd number if it is not considered lucky") do
+    luck_check(26015).should(eq(false))
   end
 end
